d56a7df50fc2e2388e4188f26886d22642efff28
sarayu sree <sarayusreeyadavpadala@gmail.com>
2023-08-20 12:34:07 +0530

    code sum of digits of a number-modified

--- gittu/PythonCodes/SumOfDigits.py
+++ gittu/PythonCodes/SumOfDigits.py
@@ -1,30 +1,38 @@
+# Python code to calculate the sum of digits of a number, by taking number input from user.
+
 import sys
 
-
 def get_integer():
-    for i in range(3,0,-1):
+    for i in range(3,0,-1):                       # executes the loop 3 times. Giving 3 chances to the user.
         num = input("enter a number:")
-        if num.isnumeric():
-            num = int(num)
+        if num.isnumeric():                       # checks if entered input is an integer string or not.
+            num = int(num)                        # converting integer string to integer. And returns it to where function is called.
             return num
         else:
-            print("enter integer only")
-            print(f'{i-1} chances are left')
-            continue
+            print("enter integer only")                    
+            if (i-1)<=1:                                   # if user entered wrong input, it displays a message and chances left.
+                print(f'{i-1} chance is left')             
+            else:
+                print(f'{i-1} chances are left')
+
+        continue   
 
 
 def addition(num):
     Sum=0
-    if type(num) is type(None):
+    if type(num) is type(None):               # Checks if number type is none or not. If type is none program exits.
         print("Try again!")
         sys.exit()
-    while num > 0:
+    while num > 0:                            # Addition- adding the digits in the number.
         digit = int(num % 10)
-        Sum = Sum + digit
-        num = num / 10
-    return Sum
+        Sum += digit
+        num /= 10
+    return Sum                                # Returns sum to where the function is called.
 
 
-number = get_integer()
-Sum = addition(number)
-print(f'sum of digits of {number} is {Sum}') 
+
+
+if __name__ == '__main__':                    # this is used to overcome the problems while importing this file.                         
+    number = get_integer()
+    Sum = addition(number)
+    print(f'Sum of digits of {number} is {Sum}')      # Prints the sum 
