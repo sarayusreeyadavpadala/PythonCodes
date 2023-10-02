7b52e13c28b543213d22cd241e46e401c7ceb515
Sarayu Sree Padala <134043600+sarayusreeyadavpadala@users.noreply.github.com>
2023-10-02 19:22:45 +0530

    Revert "I wrote the documentation (#2)"
    This reverts commit 3b6a7fab25ddc6c7fbeb71826a996256e1b96244.

--- gittu/PythonCodes/factorial.py
+++ gittu/PythonCodes/factorial.py
@@ -1,24 +1,18 @@
-
-"""This Python code calculates the factorial of a number given by the user."""
-
 import sys
 
-
 def Factorial(num):
-"""This function calculates the factorial of a number."""
-    mul = 1
+    mul=1
     for i in range(0,num):
-        if num == 0: #If the input which was given by the user is Zero,It returns One.
+        if num == 0:
             return 1
         else:
-            mul = mul*(num-i) #Else it will calculate the factorial of the number.
+            mul = mul*(num-i)
     return mul
     
 def get_input():
-"""This function gets an integer input from the user.If it was not an integer it gives three chances to the user to give a right input."""
     for i in range(3,0,-1):                      
         num = input("enter a number:")
-        if num.isnumeric():  #checks if input is numeric using .isnumeric().                 
+        if num.isnumeric():                    
             num = int(num)                      
             return num
         else:
